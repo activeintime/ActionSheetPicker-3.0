33eb9ee84f2258cf839849987afdd5b770614e55
Daniel Morgan <danmorgz@gmail.com>
2014-11-19 12:35:59 +0000

    new pool picker

---
+++ Build/Intermediates/CoreActionSheetPicker.build/Debug-iphoneos/CoreActionSheetPickerTests.build/DerivedSources/CoreActionSheetPickerTests_vers.c
@@ -0,0 +1,1 @@
+ const unsigned char CoreActionSheetPickerTestsVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:CoreActionSheetPickerTests  PROJECT:CoreActionSheetPicker-1" "\n"; const double CoreActionSheetPickerTestsVersionNumber __attribute__ ((used)) = (double)1.;
---
+++ Pickers/ActionSheetPoolDistancePicker.m
@@ -0,0 +1,13 @@
+//
+//  ActionSheetPoolDistancePicker.m
+//  ActionSheetPicker-iOS6-7
+//
+//  Created by Daniel Morgan on 19/11/2014.
+//
+//
+
+#import "ActionSheetPoolDistancePicker.h"
+
+@implementation ActionSheetPoolDistancePicker
+
+@end
