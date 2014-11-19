9297e0dd0ae1ebf5a7abffc35dc2368ddb6c0f17
Daniel Morgan <danmorgz@gmail.com>
2014-11-19 15:09:19 +0000

    pool picker work

--- Pickers/ActionSheetPoolDistancePicker.h
+++ Pickers/ActionSheetPoolDistancePicker.h
@@ -6,13 +6,24 @@
 //
 //
 
-#import <CoreActionSheetPicker/CoreActionSheetPicker.h>
+#import "AbstractActionSheetPicker.h"
+#import "DistancePickerView.h"
+
+@class ActionSheetPoolDistancePicker;
+
+typedef void(^ActionSheetPoolPickerDoneBlock)(ActionSheetPoolDistancePicker *picker, id origin);
+typedef void(^ActionSheetPoolPickerCancelBlock)(ActionSheetPoolDistancePicker *picker);
 
 @interface ActionSheetPoolDistancePicker : AbstractActionSheetPicker
 
 
-+ (id)showPickerWithTitle:(NSString *)title bigUnitString:(NSString *)bigUnitString bigUnitMax:(NSInteger)bigUnitMax selectedBigUnit:(NSInteger)selectedBigUnit smallUnitString:(NSString*)smallUnitString smallUnitMax:(NSInteger)smallUnitMax selectedSmallUnit:(NSInteger)selectedSmallUnit target:(id)target action:(SEL)action origin:(id)origin;
++ (id)showPickerWithTitle:(NSString *)title bigUnitStrings:(NSArray *)bigUnitStrings bigUnitMax:(NSInteger)bigUnitMax selectedBigUnit:(NSInteger)selectedBigUnit smallUnitStrings:(NSArray*)smallUnitStrings smallUnitMax:(NSInteger)smallUnitMax selectedSmallUnit:(NSInteger)selectedSmallUnit doneBlock:(ActionSheetPoolPickerDoneBlock)doneBlock cancelBlock:(ActionSheetPoolPickerCancelBlock)cancelBlock origin:(id)origin;
 
-- (id)initWithTitle:(NSString *)title bigUnitString:(NSString *)bigUnitString bigUnitMax:(NSInteger)bigUnitMax selectedBigUnit:(NSInteger)selectedBigUnit smallUnitString:(NSString*)smallUnitString smallUnitMax:(NSInteger)smallUnitMax selectedSmallUnit:(NSInteger)selectedSmallUnit target:(id)target action:(SEL)action origin:(id)origin;
++ (id)showPickerWithTitle:(NSString *)title bigUnitStrings:(NSArray *)bigUnitStrings bigUnitMax:(NSInteger)bigUnitMax selectedBigUnit:(NSInteger)selectedBigUnit smallUnitStrings:(NSArray*)smallUnitStrings smallUnitMax:(NSInteger)smallUnitMax selectedSmallUnit:(NSInteger)selectedSmallUnit target:(id)target action:(SEL)action origin:(id)origin;
+
+- (id)initWithTitle:(NSString *)title bigUnitStrings:(NSArray *)bigUnitStrings bigUnitMax:(NSInteger)bigUnitMax selectedBigUnit:(NSInteger)selectedBigUnit smallUnitStrings:(NSArray*)smallUnitStrings smallUnitMax:(NSInteger)smallUnitMax selectedSmallUnit:(NSInteger)selectedSmallUnit target:(id)target action:(SEL)action origin:(id)origin;
+
+-(NSInteger)bigUnits;
+-(NSInteger)smallUnits;
 
 @end
