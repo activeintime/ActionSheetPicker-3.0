1e3f3cbdbbff04342e3d235fde7e9c682c2c5c01
Daniel Morgan <danmorgz@gmail.com>
2014-11-19 18:59:25 +0000

    pool distance picker pretty much done

--- Pickers/ActionSheetPoolDistancePicker.h
+++ Pickers/ActionSheetPoolDistancePicker.h
@@ -7,7 +7,6 @@
 //
 
 #import "AbstractActionSheetPicker.h"
-#import "DistancePickerView.h"
 
 @class ActionSheetPoolDistancePicker;
 
@@ -23,7 +22,11 @@ typedef void(^ActionSheetPoolPickerCancelBlock)(ActionSheetPoolDistancePicker *p
 
 - (id)initWithTitle:(NSString *)title bigUnitStrings:(NSArray *)bigUnitStrings bigUnitMax:(NSInteger)bigUnitMax selectedBigUnit:(NSInteger)selectedBigUnit smallUnitStrings:(NSArray*)smallUnitStrings smallUnitMax:(NSInteger)smallUnitMax selectedSmallUnit:(NSInteger)selectedSmallUnit target:(id)target action:(SEL)action origin:(id)origin;
 
+- (id)initWithTitle:(NSString *)title bigUnitStrings:(NSArray *)bigUnitStrings bigUnitMax:(NSInteger)bigUnitMax selectedBigUnit:(NSInteger)selectedBigUnit smallUnitStrings:(NSArray*)smallUnitStrings smallUnitMax:(NSInteger)smallUnitMax selectedSmallUnit:(NSInteger)selectedSmallUnit doneBlock:(ActionSheetPoolPickerDoneBlock)doneBlock cancelBlock:(ActionSheetPoolPickerCancelBlock)cancelBlock origin:(id)origin;
+
 -(NSInteger)bigUnits;
 -(NSInteger)smallUnits;
+-(NSString *)selectedBigUnitString;
+-(NSString *)selectedSmallUnitString;
 
 @end
