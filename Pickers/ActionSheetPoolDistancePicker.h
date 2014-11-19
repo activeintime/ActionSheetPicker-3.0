fc185647095e2a2b958d994927843c0084254968
Daniel Morgan <danmorgz@gmail.com>
2014-11-19 12:35:59 +0000

    new pool picker

---
+++ Pickers/ActionSheetPoolDistancePicker.h
@@ -0,0 +1,13 @@
+//
+//  ActionSheetPoolDistancePicker.h
+//  ActionSheetPicker-iOS6-7
+//
+//  Created by Daniel Morgan on 19/11/2014.
+//
+//
+
+#import <CoreActionSheetPicker/CoreActionSheetPicker.h>
+
+@interface ActionSheetPoolDistancePicker : AbstractActionSheetPicker
+
+@end
